9e8b87f204477727883f5b4be5614e3a3dc7edeb
AmitGujar <amithero3342@gmail.com>
2023-11-07 15:36:42 +0530

    files updated

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "This file will be executed by Ansible on control node"
 
-file_remove() {
-if [ -f /home/amit/myPlaybooks/ansible_inventory ]; then
-    echo "Ansible inventory file exists"
-    rm ansible_inventory
-else
-    echo "Ansible inventory file does not exist"
-fi
-}
+# file_remove() {
+# if [ -f /home/amit/myPlaybooks/ansible_inventory ]; then
+#     echo "Ansible inventory file exists"
+#     rm ansible_inventory
+# else
+#     echo "Ansible inventory file does not exist"
+# fi
+# }
 
-file_remove
+# file_remove
